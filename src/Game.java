10b515c6fc7b1db7be5977be5b4f886a8a4eec3f
Sreelakshmi Chintha <schintha@Sreelakshmis-MacBook-Pro.local>
2018-07-07 03:44:19 -0700

    cleanup

--- src/Game.java
+++ src/Game.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Random;
-
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Arrays;
 
 public class Game {
 
@@ -26,7 +28,6 @@ public class Game {
             Random rand = new Random();
             int randInt = rand.nextInt(movieCount - 1);
             randomMovie = movieList.get(randInt);
-//            System.out.println(randomMovie);
         } catch (FileNotFoundException ex) {
             System.out.println("File not found");
         } catch (ArrayIndexOutOfBoundsException ex) {
@@ -35,8 +36,64 @@ public class Game {
         return randomMovie;
     }
 
-    public String maskSelectedMovie(String movie) {
-        String maskedMovie = movie.replaceAll("\\w", "-");
-        return maskedMovie;
+     public void guessMovie() {
+        boolean hasWon = false;
+        int chancesLeft = 10;
+        Map<String, List<Integer>> movieMap = new HashMap<>();
+
+        String selectedMovie = readMovies();
+        String maskedMovie = selectedMovie.replaceAll("\\w", "-");
+        char[] moviePicked = selectedMovie.toCharArray();
+//        System.out.println(selectedMovie);
+
+        for(int i=0; i<moviePicked.length; i++) {
+            if(movieMap.containsKey(Character.toString(moviePicked[i]))) {
+                movieMap.get(Character.toString(moviePicked[i])).add(i);
+            }
+            else{
+                List<Integer> indexList = new ArrayList<>();
+                indexList.add(i);
+                movieMap.put(Character.toString(moviePicked[i]), indexList);
+            }
+        }
+
+        char[] guessMovie = maskedMovie.toCharArray();
+        System.out.println(guessMovie);
+
+        System.out.println("I have randomly chosen a movie.");
+        System.out.println("Try to guess it..");
+        Scanner scanner = new Scanner(System.in);
+
+        while(!hasWon) {
+            char c = scanner.next().charAt(0);
+            if(movieMap.containsKey(Character.toString(c))) {
+                Integer index = movieMap.get(Character.toString(c)).remove(0);
+                List<Integer>  indexList = (ArrayList<Integer>) movieMap.get(Character.toString(c));
+                if(indexList.isEmpty()) {
+                    movieMap.remove(Character.toString(c));
+                }
+                guessMovie[index] = c;
+                System.out.println(guessMovie);
+            }
+            else {
+                chancesLeft -= 1;
+                if(chancesLeft == 0) {
+                    hasWon = false;
+                    break;
+                }
+                else {
+                    System.out.println("Wrong character. You have " + chancesLeft + " guess(es) left. Choose again.");
+                }
+            }
+            if(Arrays.equals(moviePicked, guessMovie)) {
+                System.out.println("You won the game");
+                hasWon = true;
+                break;
+            }
+        }
+
+        if(!hasWon) {
+            System.out.println("You lost the game. Try again");
+        }
     }
 }
--- src/Movie.java
+++ src/Movie.java
@@ -1,73 +1,11 @@
 import java.io.IOException;
-import java.util.*;
 
 public class Movie {
 
     public static void main(String[] args) throws IOException{
 
-        boolean hasWon = false;
         Game game = new Game();
-        String movieSelected = game.readMovies();
-//        movieSelected = "bird's nest";
-//        char[] withSpaces = movieSelected.toCharArray();
-//        movieSelected =  movieSelected.replaceAll("\\s","");
-        char[] moviePicked = movieSelected.toCharArray();
-        System.out.println(movieSelected);
+        game.guessMovie();
 
-        HashMap<String, List<Integer>> movieMap = new HashMap<>();
-
-        for(int i=0; i<moviePicked.length; i++) {
-            if(movieMap.containsKey(Character.toString(moviePicked[i]))) {
-                movieMap.get(Character.toString(moviePicked[i])).add(i);
-            }
-            else{
-                List<Integer> indexList = new ArrayList<>();
-                indexList.add(i);
-                movieMap.put(Character.toString(moviePicked[i]), indexList);
-            }
-        }
-
-//        movieMap.forEach((key,value) -> System.out.println(key + " = " + value));
-
-
-        String movieMasked = game.maskSelectedMovie(movieSelected);
-//        System.out.println("Masked Movie" + ' ' + movieMasked);
-        char[] guessMovie = movieMasked.toCharArray();
-        System.out.println(guessMovie);
-
-        System.out.println("I have randomly chosen a movie.");
-        System.out.println("Try to guess it..");
-
-        Scanner scanner = new Scanner(System.in);
-
-//        while(!hasWon) {
-        for(int i=20; i>0; i--) {
-            System.out.println("You have " +  i + " guess(es) left. Choose again.");
-            char c = scanner.next().charAt(0);
-
-            if(movieMap.containsKey(Character.toString(c))) {
-                Integer idx = movieMap.get(Character.toString(c)).remove(0);
-                List<Integer>  idxList = (ArrayList<Integer>) movieMap.get(Character.toString(c));
-                if(idxList.isEmpty()) {
-                    movieMap.remove(Character.toString(c));
-                }
-                guessMovie[idx] = c;
-                System.out.println(guessMovie);
-            }
-            else {
-                System.out.println("Wrong character. Choose again..");
-            }
-
-            if(Arrays.equals(moviePicked, guessMovie)) {
-                System.out.println("You won the game");
-                hasWon = true;
-                break;
-           }
-
-        }
-
-        if(!hasWon) {
-            System.out.println("You lost the game. Try again");
-        }
     }
 }
